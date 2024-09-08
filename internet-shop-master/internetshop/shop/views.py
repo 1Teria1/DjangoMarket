db89340fdc93cc1366fbdb1fb794b834fb3e9a81
1Teria1 <motia_teria@mail.ru>
2024-09-08 21:25:47 +0700

    aaaaa

---
+++ internet-shop-master/internetshop/shop/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from itertools import
+
+from .models import Product
+
+# Create your views here.
+
+
+def home(request):
+    products = Product.objects.all()
+    return render(request, "index.html", {
+        'products': products
+    })
